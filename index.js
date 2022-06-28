eb26b21faccec177d386a97f60edade319f3059b
Demaris Ashmore <demiashmore@yahoo.com>
2022-06-28 16:58:41 -0600

    commit tree 34d11f5c2812821183e543332ed725e234d9a0ef

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 38;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+describe ("Name", () => {
+    it ('returns "Susan"', () => {
+        expect (name).toEqual("Susan");
+    });
+});
+
+describe ("Height", () => {
+    it("is less than 40", () => {
+        expect (height).toBeLessThan(40);
+    });
+});
+
+describe ("Message", () => {
+    it ("gives the name and height", () => {
+        expect (message).toInclude (name);
+        expect (message).toInclude (height);
+    });
+});
